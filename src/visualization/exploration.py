8b8f01b61fed8a781e2c536a0f25bd82ae5b1a14
alexaatm <oleksandra.tmenova@surgiceye.com>
2024-02-23 11:02:48 +0100

    remove vis folder

--- src/visualization/exploration.py
+++
@@ -1,8 +0,0 @@
-import  matplotlib.pyplot as plt
-import numpy as np
-def imshow(img):
-    print(type(img))
-    img = img / 2 + 0.5 #if denormalization is needed
-    npimg = np.array(img)
-    plt.imshow(np.transpose(npimg, (1,2,0)))
-    plt.show()
